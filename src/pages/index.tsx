1254cb40ff922e43f5eafe9521feb3e1ab8121a5
Grégory Alvim <gregori.jagua@gmail.com>
2024-01-09 21:26:39 -0300

    added link from next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from '../lib/stripe'
 import Stripe from 'stripe'
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
@@ -15,7 +16,7 @@ interface HomeProps {
     name: string
     imageUrl: string
     description: string
-    price: number
+    price: string
   }[]
 }
 
@@ -30,14 +31,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        <Link key={product.id} href={`product/${product.id}`} prefetch={false}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   )
